ee0fdb3b8ed042b88c4bca7036c7990ed3067115
JamiliViana <jamilimartucheliviana@gmail.com>
2023-12-01 14:24:22 -0300

    feat: create task model

---
+++ src/main/java/com/jamili/taskmanager/model/Task.java
@@ -0,0 +1,34 @@
+package com.jamili.taskmanager.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Date creationDate;
+
+    @Column(nullable = false)
+    private String status;
+}
